4a2702fb91592a2604ba2d82d665e829d06ad241
omh1004 <ohchef2020@gmail.com>
2022-10-24 10:02:23 +0900

    입력값대소관계

---
+++ practice/src/practice/Main2.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+
+
+        while(st.hasMoreTokens()){
+            int temp = Integer.parseInt(st.nextToken());
+
+        }
+
+
+
+
+    }
+}

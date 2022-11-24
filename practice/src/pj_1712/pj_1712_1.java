1cec79c8c7ba3740ecf21e1309828357e037ea88
omh1004 <ohchef2020@gmail.com>
2022-11-24 16:01:48 +0900

    백준_손익분기점

---
+++ practice/src/pj_1712/pj_1712_1.java
@@ -0,0 +1,24 @@
+package pj_1712;
+
+import java.util.Scanner;
+
+public class pj_1712_1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int C = in.nextInt();
+		
+
+		if(B>= C) {
+			System.out.println("-1"); 
+		}
+		else {
+		 System.out.println((A/(C-B))+1); 
+		}
+		
+		
+
+	}
+}
